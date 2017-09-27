a2bacbf82cfc50aedc3e2969903b0ef112e7d34e
Jens Raaby <jensraaby@users.noreply.github.com>
2017-09-27 21:33:39 +0100

    Ensure finagle server wired up resiliently

--- rest/build.sbt
+++ rest/build.sbt
@@ -2,5 +2,6 @@ libraryDependencies ++= Seq(
   "com.github.finagle" %% "finch-core" % "0.16.0-M2",
   "com.github.finagle" %% "finch-circe" % "0.16.0-M2",
   "com.twitter" %% "twitter-server" % "1.32.0",
+  "com.twitter" %% "finagle-stats" % "7.1.0",
   "io.circe" %% "circe-generic" % "0.9.0-M1"
 )
--- rest/src/main/scala/com/jensraaby/rest/Server.scala
+++ rest/src/main/scala/com/jensraaby/rest/Server.scala
@@ -1,11 +1,12 @@
 package com.jensraaby.rest
 
+import com.twitter.finagle.http.service.HttpResponseClassifier
 import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.stats.{Stat, StatsReceiver}
 import com.twitter.finagle.{Http, Service}
-import com.twitter.finagle.param.Stats
-import com.twitter.finagle.stats.StatsReceiver
 import com.twitter.server.TwitterServer
 import com.twitter.util.{Await, Duration}
+import io.finch.Text.Plain
 import io.finch._
 
 object Server extends TwitterServer {
@@ -13,10 +14,16 @@ object Server extends TwitterServer {
   val api: Service[Request, Response] = new Api(statsReceiver)
 
   val port = flag("http.port", ":8081", "Address port to bind to, prefixed by :")
+
+  override protected val failfastOnFlagsNotParsed = true
+
   def main(): Unit = {
     val server = Http.server
-      .configured(Stats(statsReceiver))
+      .withLabel("Rest-server")
+      .withStatsReceiver(statsReceiver)
+      .withTransport.verbose
       .withAdmissionControl.concurrencyLimit(maxConcurrentRequests = 1000, maxWaiters = 10)
+      .withResponseClassifier(HttpResponseClassifier.ServerErrorsAsFailures)
       .withRequestTimeout(Duration.fromMilliseconds(500))
       .serve(port(), api)
 
@@ -30,8 +37,20 @@ object Server extends TwitterServer {
 }
 
 class Api(statsReceiver: StatsReceiver) extends Service[Request, Response] {
-  val root: Endpoint[String] = get(/) { Ok("hello") }
-  val ping: Endpoint[String] = get("ping") { Ok("pong") }
+  // Metrics to track
+  val pingCounter = statsReceiver.counter("ping")
+  val latency: Stat = statsReceiver.stat("latency")
 
-  override def apply(request: Request) = (root :+: ping).toServiceAs[Text.Plain].apply(request)
+  val root: Endpoint[String] = get(/) {
+    Stat.time(latency) { Ok("hello") }
+  }
+
+  val ping: Endpoint[String] = get("ping") {
+    pingCounter.incr()
+    Ok("pong")
+  }
+
+  val service = (root :+: ping).toServiceAs[Plain]
+
+  override def apply(request: Request) = service.apply(request)
 }
